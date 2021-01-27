06da973da3bbfcd97f8dbc51235f827d134b086b
julian <Joolean14>
2021-01-26 19:41:27 -0500

    [Clase] Duda

---
+++ src/helpers.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+import React from 'react'
+
+export default function helpers() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+// const getGiHubUserWithAxios = async () => {
+//     const response = await axios.get(gitHubUrl);
+//     setUserData(response.data);
+//   };
+
+// https://www.themealdb.com/api/json/v1/1/categories.php
