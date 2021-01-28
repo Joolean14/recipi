434175410223504693cedfba0c04669541371944
julian <Joolean14>
2021-01-28 14:35:30 -0500

    [Bootstrap] Agrego main container y card.

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from '../components/navbar/Navbar';
+import Contenedorgeneral from '../components/Contenedorgeneral/Contenedorgeneral';
+
 function App() {
   return (<Router>
     <div className="">
@@ -10,7 +13,8 @@ function App() {
           <Example/>
         </Route> */}
         <Route path='/home'>
-          <Navbar/>
+          <Navbar />
+          <Contenedorgeneral />
           {/* <Banner/>
           <Messages/>
           <Footer/> */}
